5e86716329ef9935c224535e0e217ea058719448
JTong <eldertong@gmail.com>
2018-01-03 10:19:34 -0500

    added relevant text to controller with joe and adrian

--- spec/controllers/oranges_controller_spec.rb
+++ spec/controllers/oranges_controller_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe OrangesController, type: :controller do
+RSpec.describe OrangesController do
+    context '#index' do
+      it "renders the index view" do
+        get :index
+        expect(response).to render_template("index")
+      end
+    
+      it "renders html" do
+        process :index, method: :get
+        expect(response.content_type).to eq "text/html"
+      end
+    end
+  end
 
 end
