4022ecd727d73e09b1b84e365e3e4a555dd680b3
misturfixit <jpitdesign@gmail.com>
2018-01-03 13:31:24 -0500

    pausing this tutorial to work on preceding one,,, getting more instruction on routing

--- app/controllers/oranges_controller.rb
+++ app/controllers/oranges_controller.rb
@@ -1,2 +1,11 @@
 class OrangesController < ApplicationController
+
+   def index
+  	render(:template => 'index')
+  	#can also be typed shorthand as: render('demo/hello')
+  	#Can be shortened even further as: render('hello') since 
+  	#we are in the demo controller rails will assume we mean 
+  	#demo folder of view 
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+  # 
+  get 'oranges/:index', to: 'views#index', as: 'index' 
+  
+  
+  root :to => "views#index"
+  
+
+  # match ':controller(/:action(/:id(.:format)))', :via => :get
+  end
