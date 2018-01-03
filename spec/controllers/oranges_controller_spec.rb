1f4281cee367d0dded461e241afa21ab94f07c90
ad3adp03t <ad3adp03t@gmail.com>
2018-01-03 11:47:13 -0500

    working on fixing routing issues with joe and jeremy

--- spec/controllers/oranges_controller_spec.rb
+++ spec/controllers/oranges_controller_spec.rb
@@ -1,17 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe OrangesController do
-    context '#index' do
-      it "renders the index view" do
-        get :index
-        expect(response).to render_template("index")
-      end
+  context '#index' do
+    it "renders the index view" do
+      get :index
+      expect(response).to render_template("index")
+    end
     
-      it "renders html" do
-        process :index, method: :get
-        expect(response.content_type).to eq "text/html"
-      end
+    it "renders html" do
+      process :index, method: :get
+      expect(response.content_type).to eq "text/html"
     end
   end
-
 end
